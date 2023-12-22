2d6d71959efa53cd722c76668bc2bc6ad06d8a52
Renan Claude <renanclaude.rcg@gmail.com>
2023-12-22 20:13:10 -0300

    Creating Discovery Server with Eureka + Creating Microservice in Eureka Service

---
+++ msclient/src/test/java/io/github/microserviceproject/msclient/MsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.microserviceproject.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
