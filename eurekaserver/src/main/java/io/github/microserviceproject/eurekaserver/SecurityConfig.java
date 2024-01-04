bb64901a3aff2e7c5aa0164870842fb97f3c82a0
Renan Claude <renanclaude.rcg@gmail.com>
2024-01-03 21:24:03 -0300

    Implementing authentication on all eureka clients

--- eurekaserver/src/main/java/io/github/microserviceproject/eurekaserver/SecurityConfig.java
+++ eurekaserver/src/main/java/io/github/microserviceproject/eurekaserver/SecurityConfig.java
@@ -10,8 +10,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-        .csrf().disable()
-        .authorizeRequests().anyRequest().authenticated()
+        .csrf()
+        .disable()
+        .authorizeRequests()
+        .anyRequest()
+        .authenticated()
+//        .permitAll()
         .and()
         .httpBasic();
 
